ff7d4887d593aa79f5e405d83d8a9363da9bb58c
benetsugarboy <benet1one@gmail.com>
2025-07-28 13:45:16 +0200

    Added get_user()

--- api.R
+++ api.R
@@ -2,11 +2,18 @@
 library(httr2)
 base_url <- "https://api.mcsrranked.com/"
 
-get_match <- function(api_id) {
+get_match <- function(id) {
     request(base_url) |>
-        req_url_path("matches", api_id) |>
+        req_url_path("matches", id) |>
         req_perform() |>
         resp_body_json() |>
         _$data
 }
 
+get_user <- function(uuid) {
+    request(base_url) |>
+        req_url_path("users", uuid) |>
+        req_perform() |>
+        resp_body_json() |>
+        _$data
+}

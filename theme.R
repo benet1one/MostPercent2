670acca0028cfa6c56fe9eba1545b379c07d1242
benetsugarboy <benet1one@gmail.com>
2025-07-29 12:59:06 +0200

    Plotgress

---
+++ theme.R
@@ -0,0 +1,17 @@
+
+scale_most <- RColorBrewer::brewer.pal(11, "RdBu")[c(2, 3, 4, 9, 10, 11)]
+windowsFonts(most = "Roboto Mono")
+
+theme_most <- function(...) {
+    theme_minimal(base_family = "most") +
+        theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
+}
+
+format_hms <- function(x, h = TRUE, s = TRUE) {
+    x <- hms::as_hms(x) |> format()
+    if (!h)
+        x <- stringr::str_remove(x, "^\\d{2}\\:")
+    if (!s)
+        x <- stringr::str_remove(x, "\\:\\d{2}$")
+    return(x)
+}
--- timelines.R
+++ timelines.R
@@ -1,27 +1,86 @@
 
 library(dplyr)
-matches <- readRDS("data/matches.RDS")
+library(ggplot2)
+source("theme.R")
 
-advancement_timeline <- function(match_id, timeline, standings, timepoints = seq(0, 3600, 60)[-1]) {
-    expand.grid(player = unique(timeline$player), time = timepoints) |>
-        tibble() |>
-        rowwise() |>
-        mutate(n_advancements = advancements_by(timeline, standing, time)) |>
-        mutate(match_id = match_id, .before = 1L) |>
-        ungroup() |>
-        left_join(select(standings, player, standing))
+timelines <- readRDS("data/timelines.RDS")
+standings <- readRDS("data/standings.RDS")
+    
+gtl_plot <- timelines |>
+    filter(time <= 12 * 60 * (7 - standing), is.na(ranked_event)) |>
+    mutate(standing = factor(standing)) |>
+    ggplot(aes(x = time, y = n_advancements, color = standing)) +
+    geom_point(alpha = 0.3, size = 0.8) +
+    geom_smooth(alpha = 0) +
+    scale_x_time(name = "Time", breaks = 12 * 60 * (1:5), labels = ~format_hms(.x, s = FALSE)) + 
+    scale_y_continuous(name = "Advancements", minor_breaks = NULL) +
+    scale_color_brewer(name = "Standing", type = "div", palette = 4) +
+    theme_most()
+
+plot(gtl_plot)
+
+
+timeline_plot <- function(match_id) {
+    st <- standings |>
+        filter(match_id == !!match_id)
+    timelines |>
+        filter(match_id == !!match_id) |>
+        mutate(player = factor(player, levels = st$player)) |>
+        ggplot(aes(x = time, y = n_advancements, color = player)) +
+        geom_step(show.legend = FALSE) +
+        # geom_text(data = st, aes(label = player), show.legend = FALSE,
+        #           hjust = 0, nudge_x = 20) +
+        scale_x_time(name = "Time", breaks = 12 * 60 * (1:5), limit = c(0, 3950),
+                     labels = ~format_hms(.x, s = FALSE)) + 
+        scale_y_continuous(name = "Advancements", minor_breaks = NULL) +
+        scale_color_manual(values = rev(scale_most)) +
+        theme_most()
 }
 
-advancements_by <- function(timeline, standing, time) {
-    timeline |>
-        filter(is.na(ranked_event), 
-               standing == !!standing, 
-               round(time, 1) <= !!time) |>
-        nrow()
-}
+all_timelines <- purrr::map(matches$match_id, timeline_plot) |>
+    cowplot::plot_grid(plotlist = _, ncol = 3)
 
 
-advancements <- matches |>
-    select(match_id, timeline, standings) |>
-    rowwise() |>
-    mutate(adv_timeline = list(advancement_timeline(match_id, timeline, standings)))
+splits <- tribble(
+    ~advancement, ~split, ~color,
+    "-",                    "Overworld",  "#4e6932",
+    "We Need to Go Deeper", "Nether",     "#703131",
+    "Those Were the Days",  "Bastion",    "#27232b",
+    "A Terrible Fortress",  "Fortress",   "#4a252b",
+    "Eye Spy",              "Stronghold", "#485734",
+    "The End?",             "End",        "#9c966d",
+    "Remote Getaway",       "Outer End",  "gray20",
+    # "The City at the End of the Game",  "End City", "#8c648b"
+) |> mutate(split_ind = 0:6, split = factor(split, levels = split))
+
+splitted <- readRDS("data/timelines.RDS") |>
+    left_join(select(splits, advancement, split_ind), by = "advancement") |>
+    within(split_ind[is.na(split_ind)] <- 0L) |>
+    group_by(match_id, player) |>
+    mutate(split_ind = cummax(split_ind)) |>
+    left_join(select(splits, split_ind, split), by = "split_ind") |>
+    select(!split_ind)
+    # filter(match_id == "1A1") |>
+    # print(n = 500)
+
+
+split_plot <- splitted |>
+    mutate(advancement = factor(advancement, levels = splits$advancement)) |>
+    filter(!is.na(advancement)) |>
+    group_by(standing, advancement) |>
+    summarise(n_advancements = round(median(n_advancements)),
+              tl = quantile(time, 0.35),
+              tu = quantile(time, 0.65), 
+              tm = (tl + tu) / 2,
+              n = n(), .groups = "drop") |>
+    filter(n >= 3) |>
+    ggplot(aes(x = tm, y = factor(standing), fill = advancement, color = advancement)) +
+    geom_segment(aes(x = tl, xend = tu), linewidth = 6, alpha = 0.5) +
+    geom_text(aes(label = n_advancements), nudge_y = 0.3, family = "most", show.legend = FALSE) +
+    scale_x_time(name = "Time", labels = ~format_hms(.x, s = FALSE)) + 
+    scale_y_discrete(name = "Standing") +
+    scale_color_manual(name = "Split", values = splits$color[-1]) +
+    theme_most()
+
+plot(split_plot)
+

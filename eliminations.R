0a0b511eb7ec53091d60ffde02c0b1f67e6610e8
benetsugarboy <benet1one@gmail.com>
2025-07-31 17:20:31 +0200

    Improved Plot

--- eliminations.R
+++ eliminations.R
@@ -53,17 +53,19 @@ difference_plot <- difference_summary |>
     annotate("segment", color = line_color, linewidth = 0.9, x = 12 * 60 * 1:5, y = 6:2, yend = 1) +
     annotate("point", color = line_color, size = 4, x = 12 * 60 * 1:5, y = 6:2) +
     geom_point(aes(size = mean, color = mean)) +
-    geom_text(aes(label = format(mean, nsmall = 1, digits = 1)), 
-              color = "white", family = "bold") +
+    geom_text(aes(label = format(mean, nsmall = 1, digits = 1)), color = "white", family = "bold") +
+    geom_text(aes(label = round(tiebreak_prop*100) |> paste0("%")), 
+              hjust = 0, nudge_x = 60 * 4, alpha = 0.6, size = 3, family = "bold") +
     scale_x_time(name = "", labels = ~format_hms(.x, s = FALSE),
-                 breaks = 12 * 60 * (1:5), minor_breaks = NULL, limits = 60 * c(10, 62)) +
-    scale_y_reverse(name = "Standings", breaks = 1:6, limits = c(6.2, 0.5), labels = format_standings) +
+                 breaks = 12 * 60 * (1:5), minor_breaks = NULL, limits = 60 * c(10, 65)) +
+    scale_y_reverse(name = "Standings", breaks = 1:6, limits = c(6.2, 0.6), labels = format_standings) +
     scale_color_gradient(low = scale_most[2], high = scale_most[5]) +
-    scale_size(range = c(13, 19)) +
-    ggtitle("Average Margin of Elimination", "Advancement difference between eliminated player and survivors") +
+    scale_size(range = c(12, 19)) +
+    ggtitle("Average Margin of Elimination", 
+            "Advancement difference between eliminated player and survivors \nPercentage of tiebreaks") +
     theme_most() +
     theme(
-        aspect.ratio = 1,
+        aspect.ratio = 0.88,
         panel.grid = element_blank(),
         legend.position = "none"
     )
